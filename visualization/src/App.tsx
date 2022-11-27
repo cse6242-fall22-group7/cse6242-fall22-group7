eac4664ff9ea4893f0376e88ac82be7005f0a2e1
Michael Tang <michaeltang6649@gmail.com>
2022-11-27 11:58:50 -0500

    update

--- visualization/src/App.tsx
+++ visualization/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { ForceGraph3D } from "react-force-graph";
 import neo4j, { Driver, QueryResult } from 'neo4j-driver';
 import { utils } from 'ethers';
@@ -13,7 +13,8 @@ type AddressNode = {
 type Link = {
   source: string,
   target: string,
-  value: number,
+  count: number,
+  total_amount: number,
 }
 
 type GraphData = {
@@ -21,20 +22,31 @@ type GraphData = {
   links: Array<Link>,
 }
 
+type AddressDetail = {
+  total_received: number,
+  total_sent: number,
+  num_received: number,
+  num_sent: number,
+}
+
 const TYPE1_QUERY = `
-MATCH (source:Address {type: 1} )-->(target:Address) RETURN source, target
+MATCH (source:Address {type: 1} )-[r:SENT]-(target:Address) RETURN source, r, (startNode(r) = source) as isFromSource, target
 ORDER BY source.type DESC, target.type DESC
 LIMIT $limit
 `;
 
 const ADDRESS_QUERY = `
-MATCH (source:Address {addr: $address})--(target:Address) return source, target
+MATCH (source:Address {addr: $address})-[r:SENT]-(target:Address) RETURN source, r, (startNode(r) = source) as isFromSource, target
 LIMIT $limit
 `;
 
 const ALL_ADDRESS_QUERY = `
-MATCH (source:Address)-->(target:Address) return source, target
+MATCH (source:Address)-[r:SENT]-(target:Address) RETURN source, r, (startNode(r) = source) as isFromSource, target
 LIMIT $limit
+`
+
+const ADDRESS_DETAILS_QUERY = `
+MATCH (source:Address {addr: $address} )-[r:SENT]-(:Address) RETURN (startNode(r) = source) as isFromSource, sum(r.count) as count, sum(r.total_amount) as total_amount;
 `
 
 export default function App() {
@@ -46,10 +58,17 @@ export default function App() {
 
   const [data, setData] = useState<GraphData>({ nodes: [], links: [] });
   const [addressesMap, setAddressesMap] = useState<Map<string, AddressNode>>();
+  const [clickedAddressDetail, setClickedAddressDetail] = useState<AddressDetail>();
   const [selectedAddress, setSelectedAddress] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [recordLimit, setRecordLimit] = useState(1000);
   const [hoveredAddress, setHoveredAddress] = useState<AddressNode | null>(null);
   const ref = useRef();
+
+  const handleError = (e: any) => {
+    alert(e);
+    setIsLoading(false);
+  }
 
   const handleData = (result: QueryResult<Dict<PropertyKey, any>>) => {
     if (!result.records.length) {
@@ -57,10 +76,17 @@ export default function App() {
       return;
     }
     const links: Array<Link> = result.records.map((r) => {
+      if (r.get("isFromSource") === true) {
+        var source = r.get("source"); var target = r.get("target");
+      } else {
+        var source = r.get("target"); var target = r.get("source");
+      }
+      const rel = r.get("r")
       return {
-        source: r.get("source").properties.addr,
-        target: r.get("target").properties.addr,
-        value: 1,
+        source: source.properties.addr,
+        target: target.properties.addr,
+        total_amount: rel.properties.total_amount,
+        count: rel.properties.count,
       }
     });
     const addresses = new Map<string, AddressNode>();
@@ -71,7 +97,6 @@ export default function App() {
       addresses.set(addr, { id: addr, type: type });
     });
     setAddressesMap(addresses);
-    // setHoveredAddress(null);
     setData(
       {
         nodes: Array.from(addresses.values()),
@@ -106,21 +131,22 @@ export default function App() {
       return;
     }
     var session = driver.session({ defaultAccessMode: neo4j.session.READ });
-    session.run(ADDRESS_QUERY, { address: selectedAddress, limit: neo4j.int(recordLimit) })
-      .then(handleData).then(() => { session.close() });
+    setIsLoading(true);
+    session.run(ADDRESS_QUERY, { address: selectedAddress.toLowerCase(), limit: neo4j.int(recordLimit) })
+      .then(handleData).then(() => { session.close(); setIsLoading(false); }).catch(handleError);
   }
 
   const queryAllAddresses = () => {
     var session = driver.session({ defaultAccessMode: neo4j.session.READ });
+    setIsLoading(true);
     session.run(ALL_ADDRESS_QUERY, { limit: neo4j.int(recordLimit) })
-      .then(handleData).then(() => { session.close() });
+      .then(handleData).then(() => { session.close(); setIsLoading(false); }).catch(handleError);
   }
 
-  const queryHoveredAddress = () => {
+  const expandSelectedAddress = () => {
     if (hoveredAddress) {
+      setIsLoading(true);
       var session = driver.session({ defaultAccessMode: neo4j.session.READ });
-      // session.run(ADDRESS_QUERY, { address: hoveredAddress.id, limit: neo4j.int(recordLimit) })
-      //   .then(handleData).then(() => { session.close() });
       session.run(ADDRESS_QUERY, { address: hoveredAddress.id, limit: neo4j.int(recordLimit) })
         .then(
           (result) => {
@@ -130,14 +156,14 @@ export default function App() {
             result.records.forEach(record => {
               var addLink = true;
 
-              var { addr, type } = record.get("source").properties;
+              var { addr, type } = record.get("isFromSource") ? record.get("source").properties : record.get("target").properties;
               var sAddr = addr;
               if (!addresses.has(addr)) {
                 addresses.set(addr, { id: addr, type: type });
                 addItems.push(addresses.get(addr)!);
                 addLink = true;
               }
-              var { addr, type } = record.get("target").properties;
+              var { addr, type } = !record.get("isFromSource") ? record.get("source").properties : record.get("target").properties;
               var tAddr = addr;
               if (!addresses.has(addr)) {
                 addresses.set(addr, { id: addr, type: type });
@@ -145,8 +171,9 @@ export default function App() {
                 addLink = true;
               }
               if (addLink) {
-                addLinks.push({ source: sAddr, target: tAddr, value: 1 });
+                addLinks.push({ source: sAddr, target: tAddr, count: record.get("r").properties.count, total_amount: record.get("r").properties.total_amount });
               }
+
             });
             if (addItems.length) {
               setData(
@@ -159,27 +186,54 @@ export default function App() {
               alert("No new connections found");
             }
           }
-        ).then(() => { session.close() });
+        ).then(() => { session.close(); setIsLoading(false); }).catch(handleError);
     }
   }
 
   const queryAbnormalAddresses = () => {
     var session = driver.session({ defaultAccessMode: neo4j.session.READ });
+    setIsLoading(true);
     session.run(TYPE1_QUERY, { limit: neo4j.int(recordLimit) })
-      .then(handleData).then(() => { session.close() });
+      .then(handleData).then(() => { session.close(); setIsLoading(false); }).catch(handleError);
   }
 
   const showNodeLabel = (node: any) => {
     return `${utils.getAddress(node.id)}`;
   }
 
-  const handleNodeClick = (node: any) => {
+  const showLinkLabel = (link: any) => {
+    return `# TXs: ${link.count}, Amt: ${link.total_amount}ETH`
+  }
+
+  const handleNodeClick = useCallback((node: any) => {
     if (node) {
+      var session = driver.session({ defaultAccessMode: neo4j.session.READ });
+      setIsLoading(true);
+      session.run(ADDRESS_DETAILS_QUERY, { address: node.id })
+        .then((result) => {
+          var total_received = 0, total_sent = 0;
+          var num_received = 0, num_sent = 0;
+          result.records.forEach(record => {
+            record.get("isFromSource") ? total_sent = record.get("total_amount") : total_received = record.get("total_amount");
+            record.get("isFromSource") ? num_sent = record.get("count") : num_received = record.get("count");
+            setClickedAddressDetail(
+              { total_received: total_received, total_sent: total_sent, num_received: num_received, num_sent: num_sent }
+            )
+          });
+        }).then(() => { session.close(); setIsLoading(false); }).catch(handleError);
+
+      const distance = 500;
+      const distRatio = 1 + (distance) / Math.hypot(node.x, node.y, node.z);
+      (ref.current! as any).cameraPosition(
+        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio },
+        node,
+        500
+      );
       setHoveredAddress({ id: node.id, type: node.type });
     } else {
       setHoveredAddress(null);
     }
-  }
+  }, [ref]);
 
   return (
     <div className="app">
@@ -191,6 +245,7 @@ export default function App() {
           cooldownTime={10000}
           linkWidth={1}
           nodeLabel={showNodeLabel}
+          linkLabel={showLinkLabel}
           nodeColor={(node: any) => (selectedAddress === node.id || hoveredAddress?.id === node.id) ? "red" : node.type === 1 ? "orange" : "lightblue"}
           onNodeClick={handleNodeClick}
           linkDirectionalArrowLength={3}
@@ -233,14 +288,38 @@ export default function App() {
             left: "32px",
             zIndex: 3,
           }}>
-          <div>{hoveredAddress.id}</div>
+          <div style={{ fontFamily: "monospace", fontSize: "20px" }}>{utils.getAddress(hoveredAddress.id)}</div>
+          {
+            clickedAddressDetail && (
+              <div>
+                {clickedAddressDetail.num_received > 0 && (<div>Received {clickedAddressDetail.num_received} times, total {clickedAddressDetail.total_received.toFixed(4)} ETH</div>)}
+                {clickedAddressDetail.num_sent > 0 && (<div>Sent {clickedAddressDetail.num_sent} times, total {clickedAddressDetail.total_sent.toFixed(4)} ETH</div>)}
+              </div>
+            )
+          }
           <div> {hoveredAddress.type === 1 ? "Abnormal" : "Normal"} Address
+          </div>
+          <div>
             <button onClick={() => { window.open(`https://etherscan.io/address/${hoveredAddress.id}`, '_blank'); }}>
               Open in Etherscan </button>
-            <button onClick={() => { queryHoveredAddress(); }}>Expand this node</button>
+            <button onClick={() => { expandSelectedAddress(); }}>Expand this node</button>
           </div>
         </div>
       )}
+      {isLoading && (
+        <div className="overlay"
+          style={{
+            position: "absolute",
+            bottom: "100px",
+            right: "32px",
+            zIndex: 4,
+          }}>
+          <div className="spinner-container">
+            <div className="loading-spinner"></div>
+          </div>
+        </div>
+      )
+      }
     </div>
   );
 }
